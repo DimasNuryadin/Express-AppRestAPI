fdc1fbf4db52aab4bc8ac62a20fb5b4a9ebf1cae
DimasNuryadin <dimasnuryadinn@gmail.com>
2021-12-14 12:36:02 +0700

    membuat file

---
+++ controller.js
@@ -0,0 +1,9 @@
+// Untuk code dijalankan lebih ketat
+'use strict';
+
+var response = require('./res');
+var connection = require('./koneksi');
+
+exports.index = function(req,res) {
+    response.ok("Aplikasi REST API ku berjalan")
+}
---
+++ koneksi.js
@@ -0,0 +1,17 @@
+// Memanggil library dari mysql
+var mysql = require('mysql');
+
+//Buat Koneksi database
+const conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'dbrestapi'
+})
+
+conn.connect((err) => {
+    if(err) throw err;
+    console.log('Mysql terkoneksi');;
+});
+
+module.exports = conn;
