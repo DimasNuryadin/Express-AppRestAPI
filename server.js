778795abb3d8fc18df61ce9b4c54c0d9347cef7d
DimasNuryadin <dimasnuryadinn@gmail.com>
2021-12-14 12:40:53 +0700

    setting route di server.js

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 // Ketik bodyparser.url dan bodyparser.json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// Panggil Routes
+var routes = require('./router');
+routes(app);
+
 app.listen(3000, () => {
     console.log('Server started on port');
 })
